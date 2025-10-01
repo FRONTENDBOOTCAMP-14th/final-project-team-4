eacc4612a1b2433cb0e98f431b7640a6e3cf660d
chajiiiii <j.cha1708@gmail.com>
2025-10-01 14:32:25 +0900

    init: 프로젝트 초기 설정

---
+++ src/app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next"
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Minimo",
+    // 앱의 전체 이름 (설치 시 표시)
+    short_name: "Minimo",
+    // 홈 화면에 표시될 짧은 이름
+    description:
+      "An interactive social platform for small challenges, achievements, and sharing.",
+    start_url: "/",
+    display: "standalone",
+    // "standalone"으로 설정하면 브라우저 UI 없이 앱처럼 실행
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      // 유저 디바이스에 다운로드 될 아이콘
+      {
+        src: "/test.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/test.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  }
+}
