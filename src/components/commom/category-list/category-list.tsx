fd068bc9c3901e7015562fc81b6e173e42d43bf5
chacokyo <3jisoo5@naver.com>
2025-10-12 22:57:51 +0900

    refactor: 다중 선택과 단일 선택이 가능하게 리펙토링
    - 기존엔 checkbox로 옵션을 제어하여 다중 선택이 가능했다면,
    radio 타입으로 단일 선택도 가능한 환경으로 구현
    
    - 분리했던 CategoryListItem를 해당 파일로 이동하여 하나의 컴포넌트로 관리

--- src/components/commom/category-list/category-list.tsx
+++ src/components/commom/category-list/category-list.tsx
@@ -1,17 +1,24 @@
-import CategoryListItem from "./category-list-item"
 import styles from "./category-list.module.css"
 
 interface CategoryListProps {
   title: string
   labels: string[]
+  inputType: "checkbox" | "radio"
 }
 
-export default function CategoryList({ title, labels }: CategoryListProps) {
+export default function CategoryList({
+  title,
+  labels,
+  inputType,
+}: CategoryListProps) {
   return (
-    <fieldset className={styles.wrapper}>
+    <fieldset className={styles.container}>
       <legend>{title}</legend>
       {labels.map((label) => (
-        <CategoryListItem label={label} key={label} />
+        <div className={styles.wrapper} key={label}>
+          <input type={inputType} id={label} name="categoryList" />
+          <label htmlFor={label}>{label}</label>
+        </div>
       ))}
     </fieldset>
   )
