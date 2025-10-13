c40f6534fd36c1a0fa82e4a180c4af6c14adf283
chacokyo <3jisoo5@naver.com>
2025-10-14 04:29:58 +0900

    feat: 헤더 컴포넌트 UI 초기 구현
    - pc 환경에서의 UI 구현 진행 완료
    - tablet,mobile 구현 예정

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
+import Navigation from "@/components/common/navigation/navigation"
+import styles from "./header.module.css"
+
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="미니모 홈으로 이동"
+          width={100}
+          height={76}
+          priority={true}
+        />
+      </Link>
+      <Navigation />
     </header>
   )
 }
-
