3ab58dc225d5cc444d5aead197775695b18cc7d7
wjinss <aastu4343@gmail.com>
2025-10-14 17:42:40 +0900

    feat: 목데이터 호출 및 랜더링, 마크업 수정,

--- src/app/challenges/[id]/page.tsx
+++ src/app/challenges/[id]/page.tsx
@@ -1,10 +1,58 @@
 import Image from "next/image"
 import Button from "@/components/common/button/button"
-// import ChallengeCardList from "@/components/common/challenge-card-list/challenge-card-list"
+import ChallengeCardList from "@/components/common/challenge-card-list/challenge-card-list"
+import AvatarLink from "@/components/user/avatar-link/avatar-link"
+import type { Database } from "@/libs/supabase/database.types"
+import { createClient } from "@/libs/supabase/server"
 import testImg from "./assets/test.avif"
 import styles from "./page.module.css"
 
-export default async function ChallengeDetail() {
+export type Challenge = Database["public"]["Tables"]["challenges"]["Row"]
+export type User = Database["public"]["Tables"]["users"]["Row"]
+
+export default async function ChallengeDetailPage({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const supabase = await createClient()
+
+  // 챌린지 데이터 호출
+  const { data: challenge, error: challengeError } = await supabase
+    .from("challenges")
+    .select("*")
+    .eq("id", params.id)
+    .single<Challenge>()
+
+  if (challengeError || !challenge) {
+    console.error("챌린지 정보를 불러오지 못했습니다:", challengeError)
+    return <p>챌린지 데이터를 불러올 수 없습니다 😢</p>
+  }
+
+  // 유저 데이터 호출
+  const { data: users, error: userError } = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", challenge.created_by_id)
+    .single<User>()
+
+  if (userError || !users) {
+    console.error("유저 정보를 불러오지 못했습니다:", userError)
+    return <p>유저 데이터를 불러올 수 없습니다 😢</p>
+  }
+
+  // 챌린지 진행 기간 계산
+  const getDateDiff = (startDate, endDate) => {
+    const date1 = new Date(startDate)
+    const date2 = new Date(endDate)
+
+    const diffDate = date2.getTime() - date1.getTime()
+
+    if (diffDate) {
+    }
+    return Math.floor(diffDate / (1000 * 60 * 60 * 24))
+  }
+
   return (
     <main className={styles.main}>
       <figure>
@@ -12,37 +60,38 @@ export default async function ChallengeDetail() {
         <div />
       </figure>
       <div className={styles.contentWrapper}>
-        <div>습관</div>
-        <h2>하루에 10페이지 책 읽기</h2>
+        <div>{challenge.category}</div>
+        <h1>{challenge.title}</h1>
         <section className={styles.description}>
-          <h3>소개글</h3>
-          <p>
-            AI 코딩 도구를 활용하면 코드 생성 및 자동화, 개발 워크플로우와의
-            통합 등이 가능하며 기존 개발 환경 대비 생산성을 높일 수 있습니다.
-            그러나 개발자를 꿈꾸며 학습을하는 예비 개발자에게 AI 코딩 도구는
-            양날의 검이 될 수 있습니다.
-          </p>
+          <h2>소개글</h2>
+          <p>{challenge.description}</p>
           <div className={styles.tagWrapper}>
-            <span>#독서</span>
-            <span>#습관</span>
-            <span>#자기개발</span>
+            {challenge.tags.map((tag, index) => (
+              <span key={index}>#{tag}</span>
+            ))}
           </div>
         </section>
         <section className={styles.infoWrapper}>
-          <h3>챌린지 정보</h3>
+          <h2>챌린지 정보</h2>
           <div className={styles.info}>
-            <span>10일 챌린지</span>
-            <span>24명 참여중</span>
-            <span> 성공 기준: 80%</span>
+            <span>{`${getDateDiff(challenge.start_at, challenge.end_at)}일 챌린지`}</span>
+            <span>{`${challenge.participants_count}명 참여중`}</span>
+            <span>{`성공 기준: ${challenge.success_threshold_percent}%`}</span>
           </div>
-          <div className={styles.avatar}>아바타</div>
+          <div className={styles.userAvatar}>
+            <AvatarLink imageUrl={users.profile_image} />
+            <span>{users.username}</span>
+          </div>
           <div className={styles.buttonWrapper}>
             <Button className="primary">참여하기</Button>
             <Button className="like">찜하기</Button>
             <Button className="share">공유하기</Button>
           </div>
         </section>
-        {/* <ChallengeCardList title="습관" challenges={} /> */}
+        <ChallengeCardList
+          title={`${challenge.category}의 다른 챌린지`}
+          challenges={[challenge]}
+        />
       </div>
     </main>
   )
