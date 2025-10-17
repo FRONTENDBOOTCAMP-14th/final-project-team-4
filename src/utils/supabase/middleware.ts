95e38d2592fe11192d1e8e1bfaf0580312d056d2
chajiiiii <j.cha1708@gmail.com>
2025-10-17 17:16:35 +0900

    fix: 미들웨어 퍼블릭 파쓰 수정

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -38,11 +38,11 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
+  const publicPaths = ["/", "/login", "/auth", "/error"]
+
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/error")
+    !publicPaths.some((path) => request.nextUrl.pathname.startsWith(path))
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
