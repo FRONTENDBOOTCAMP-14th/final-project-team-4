c7d93dffb4b80e1345db08a8e0be7abdedcf2323
chajiiiii <j.cha1708@gmail.com>
2025-10-17 11:17:19 +0900

    fix: 버튼 컴포넌트 type 프롭스 추가

--- src/components/user/sign-out-button/sign-out-button.tsx
+++ src/components/user/sign-out-button/sign-out-button.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/common/button/button"
 
 export default function SignOutButton() {
   return (
-    <Button onClick={handleSignOut} className="report">
+    <Button onClick={handleSignOut} className="report" type="button">
       로그아웃
     </Button>
   )
