2f5908691285674371f638cc332aac97fdeed0a1
chacokyo <3jisoo5@naver.com>
2025-10-19 23:05:32 +0900

    feat: 체크박스, 라디오 타입의 인풋 컴포넌트 구현
    - 타입을 props로 전달 받으며, 기본 props로 radio 타입 설정
    - 기존에 제작된 카테고리 리스트 컴포넌트와 함께 리펙토링 예정

---
+++ src/components/input/choice-input/choice-input.tsx
@@ -0,0 +1,36 @@
+import useInputConfig from "../useInputConfig"
+import styles from "./choice-input.module.css"
+import type { InputId } from "../const"
+
+export interface ChoiceInputProps {
+  id: InputId
+  type?: "checkbox" | "radio"
+  required?: boolean
+}
+
+export default function ChoiceInput({
+  id,
+  type = "radio",
+  required = true,
+}: ChoiceInputProps) {
+  const { label, className, options } = useInputConfig(id)
+
+  return (
+    <fieldset className={styles[className]}>
+      <legend>{label}</legend>
+      {options.map((option: string, index: number) => (
+        <div key={option} className={styles.wrapper}>
+          <input
+            type={type}
+            id={option}
+            name={id}
+            defaultChecked={index === 0}
+            value={option}
+            required={required}
+          />
+          <label htmlFor={option}>{option}</label>
+        </div>
+      ))}
+    </fieldset>
+  )
+}
