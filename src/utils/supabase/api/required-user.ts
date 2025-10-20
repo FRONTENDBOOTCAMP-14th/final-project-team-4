42313a6db189cd806a18684e73f6d8fbcaae0d24
chacokyo <3jisoo5@naver.com>
2025-10-20 19:36:53 +0900

    refactor: 챌린지 생성을 위한 유저 확인 api 리펙토링
    - 여러 에러처리를 추가한 리펙토링

--- src/utils/supabase/api/required-user.ts
+++ src/utils/supabase/api/required-user.ts
@@ -1,10 +1,18 @@
-// required-user.ts
 "use client"
 import browserClient from "../client"
 import type { User } from "@supabase/supabase-js"
 
 const requiredUser = async (): Promise<User> => {
-  const { data, error } = await browserClient.auth.getUser()
+  const supabase = browserClient()
+  const session = supabase.auth.getSession()
+  const user = (await session).data?.session?.user
+  if (!user) {
+    throw new Error("로그인이 필요합니다.")
+  }
+  if (!supabase.auth) {
+    throw new Error("supabase auth가 초기화 되지 않았습니다.")
+  }
+  const { data, error } = await supabase.auth.getUser()
   if (error || !data.user) throw new Error("로그인이 필요합니다.")
   return data.user
 }
