e38141495f07e186a03f79c6ad96cba9f470b4f6
chacokyo <3jisoo5@naver.com>
2025-10-20 15:42:28 +0900

    feat: 폼 제출 이벤트 핸들러 구현

---
+++ src/app/challenges/create/hadleSubmit.ts
@@ -0,0 +1,24 @@
+import type { FormEvent } from "react"
+import type { Challenge } from "@/utils/supabase"
+import { createChallenge } from "@/utils/supabase/api/challenges"
+
+export const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  const form = e.currentTarget
+  const data = new FormData(form)
+  const isPublic = data.get("is_public") === "on"
+
+  const payload = {
+    category: data.get("category") as Challenge["category"],
+    title: data.get("title") as Challenge["title"],
+    description: data.get("description") as Challenge["description"],
+    is_public: isPublic as unknown as Challenge["is_public"],
+    tags: data.getAll("tags") as unknown as Challenge["tags"],
+    start_at: data.get("start_at") as Challenge["start_at"],
+    end_at: data.get("end_at") as Challenge["end_at"],
+    success_threshold_percent: Number(data.get("success_threshold_percent")),
+    uploading_type: data.get("uploading_type") as Challenge["uploading_type"],
+  }
+
+  await createChallenge(payload)
+}
