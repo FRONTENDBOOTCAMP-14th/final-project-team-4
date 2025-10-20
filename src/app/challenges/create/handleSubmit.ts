ee79ff1f848955df063ad5edf3e2d4e81dd272b4
chacokyo <3jisoo5@naver.com>
2025-10-20 19:37:42 +0900

    rename: 폼 제출용 핸들러 함수명 수정

--- src/app/challenges/create/handleSubmit.ts
+++ src/app/challenges/create/handleSubmit.ts
@@ -6,13 +6,14 @@ export const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
   e.preventDefault()
   const form = e.currentTarget
   const data = new FormData(form)
-  const isPublic = data.get("is_public") === "on"
 
   const payload = {
     category: data.get("category") as Challenge["category"],
     title: data.get("title") as Challenge["title"],
     description: data.get("description") as Challenge["description"],
-    is_public: isPublic as unknown as Challenge["is_public"],
+    is_public: Boolean(
+      data.get("is_public")
+    ) as unknown as Challenge["is_public"],
     tags: data.getAll("tags") as unknown as Challenge["tags"],
     start_at: data.get("start_at") as Challenge["start_at"],
     end_at: data.get("end_at") as Challenge["end_at"],
