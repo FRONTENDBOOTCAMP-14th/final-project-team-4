44b9efd161648caa239fc2ad71ee2839b44fc500
wjinss <aastu4343@gmail.com>
2025-10-22 19:46:45 +0900

    feat: 네이버 로고 svg 삽입

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -55,7 +55,15 @@ export default function Login() {
                 className={styles.naverLoginButton}
                 type="button"
               >
-                <Image src={appleIconPath.src} width={21} height={26} alt="" />
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="#fff"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={21}
+                  height={26}
+                >
+                  <path d="M16.273 12.845 7.376 0H0v24h7.727V11.155L16.624 24H24V0h-7.727v12.845z" />
+                </svg>
                 네이버로 시작하기
               </button>
             </li>
