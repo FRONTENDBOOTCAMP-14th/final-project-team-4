184579bff98c563a41881d14577f9b23fa0cf4c1
chajiiiii <j.cha1708@gmail.com>
2025-10-22 15:28:15 +0900

    test: 배포 테스트 코드 추가

--- src/app/auth/login/kakao/actions.ts
+++ src/app/auth/login/kakao/actions.ts
@@ -7,7 +7,8 @@ export default async function handleKakaoLogin() {
     provider: "kakao",
     options: {
       // 콜백 루트(OAuth 인증 후 돌아오는 주소)
-      redirectTo: `http://localhost:3000/auth/callback/kakao`,
+      // redirectTo: `http://localhost:3000/auth/callback/kakao`,
+      redirectTo: `http://final-project-team-4-ruby.vercel.app/auth/callback/kakao`,
       queryParams: {
         scope: "profile_nickname profile_image",
       },
