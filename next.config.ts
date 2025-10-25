f0db94029d47b07e64214e598e212cc0df14d363
wjinss <aastu4343@gmail.com>
2025-10-26 02:56:40 +0900

    feat: 외부 이미지 사용으로 인한 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,32 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "k.kakaocdn.net",
+        pathname: "/**",
+      },
+
+      {
+        protocol: "https",
+        hostname: "img1.kakaocdn.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "img1.kakaocdn.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "t1.kakaocdn.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "t1.kakaocdn.net",
         pathname: "/**",
       },
       {
