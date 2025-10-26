337a4cc505641bf09c873dbd0f9f865034547459
chajiiiii <j.cha1708@gmail.com>
2025-10-26 19:08:30 +0900

    feat: 유저 페이지 내 신고하기 버튼 컴포넌트 UI

---
+++ src/components/user/user-page/report-account-button/report-account-button.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Button from "@/components/common/button/button"
+
+export default function ReportAccountButton() {
+  return (
+    <Button className="report" type="button">
+      신고하기
+    </Button>
+  )
+}
