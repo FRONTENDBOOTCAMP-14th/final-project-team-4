1a3943aba1aeaafd5945ae3d5f376b6f57fa1c0e
chajiiiii <j.cha1708@gmail.com>
2025-10-27 13:45:35 +0900

    feat: no-challenges-yet 컴포넌트

---
+++ src/components/user/user-page/no-challenges-yet/no-challenges-yet.tsx
@@ -0,0 +1,21 @@
+import styles from "./no-challenges-yet.module.css"
+import type { TabType } from "../user-challenges-section/user-challenges-section"
+
+export default function NoChallengesYet({ activeTab }: { activeTab: TabType }) {
+  let message = ""
+
+  if (activeTab === "ongoing") {
+    message = "진행중인 챌린지가 없어요!"
+  } else if (activeTab === "past") {
+    message = "지난 챌린지가 없어요!"
+  } else if (activeTab === "created") {
+    message = "만든 챌린지가 없어요!"
+  }
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.mainMessage}>{message}</p>
+      <p className={styles.subMessage}>지금 바로! 챌린지에 도전해보세요!</p>
+    </div>
+  )
+}
