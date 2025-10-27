d4e91c41f0566f4f2461c07d4cf4178f7c33b75f
chajiiiii <j.cha1708@gmail.com>
2025-10-27 14:16:20 +0900

    feat: 미들웨어 리다이렉트 유저 동적 데이터 넣기

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -43,6 +43,8 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
+  console.log("내 아이디", user.id)
+
   const restrictedPaths = ["/challenges/create", "/wishlist"]
 
   if (
@@ -57,7 +59,7 @@ export async function updateSession(request: NextRequest) {
 
   if (user && request.nextUrl.pathname.startsWith("/auth/login")) {
     const url = request.nextUrl.clone()
-    url.pathname = `user/userId`
+    url.pathname = `user/${user.id}`
     return NextResponse.redirect(url)
   }
 
