42eb0a070589efb679cddc8c24d3b382ebe6c67c
chacokyo <3jisoo5@naver.com>
2025-10-28 10:10:20 +0900

    refactor: 생성페이지에 로그인 전 접근 시 리다이렉트 추가로 유사 코드 구문 제거
    - 기존에 로그인한 유저 정보를 찾는 코드 제거

--- src/app/challenges/create/page.tsx
+++ src/app/challenges/create/page.tsx
@@ -1,37 +1,7 @@
-"use client"
-
-import { useEffect } from "react"
-import Link from "next/link"
-import browserClient from "@/utils/supabase/client"
-import useUserStore from "store/userStore"
 import CreateForm from "./create-form"
 import styles from "./page.module.css"
 
 export default function ChallengeCreatePage() {
-  const { loggedInUser, isLoading, fetchLoggedInUser } = useUserStore()
-
-  useEffect(() => {
-    const client = browserClient()
-
-    const { data: listener } = client.auth.onAuthStateChange(() => {
-      void fetchLoggedInUser()
-    })
-
-    void fetchLoggedInUser()
-
-    return () => listener.subscription.unsubscribe()
-  }, [fetchLoggedInUser])
-
-  if (isLoading) return <main>로딩중...</main>
-
-  if (!loggedInUser)
-    return (
-      <main className={styles.pageWrapper}>
-        <h1>로그인이 필요합니다.</h1>
-        <Link href="/auth/login">로그인 하러 가기</Link>
-      </main>
-    )
-
   return (
     <main className={styles.pageWrapper}>
       <h1 className="sr-only">챌린지 생성하기</h1>
